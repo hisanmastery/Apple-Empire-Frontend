f465a3632eddce09833fa47a561fa4493c31e309
saif <saaifiqbal@gmail.com>
2024-03-15 03:33:46 +0600

    navbar -- Design navbar and sidebar

---
+++ src/components/shared/navbar/searchInput.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Search } from 'lucide-react';
+
+const SearchInput = () => {
+  return (
+    <div className="relative">
+      {/* <form className="relative"> */}
+      <input
+        type="text"
+        className="border-gray-300 focus:border-blue-500 focus:ring-blue-500 block w-full rounded-md pl-3 pr-10 lg:py-2"
+        placeholder="Search..."
+      />
+      <button
+        type="submit"
+        className="absolute inset-y-0 right-0 px-3 lg:py-2 border border-transparent rounded-md bg-transparent text-[#FF4C06] hover:text-[#1E272E] focus:outline-none focus:text-[#1E272E] "
+      >
+        <Search />
+      </button>
+      {/* </form> */}
+    </div>
+  );
+};
+
+export default SearchInput;
