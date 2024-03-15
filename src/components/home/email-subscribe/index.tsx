2ed6bf6ec0680c9dfef25e8b1b3018834a458c1f
abcmehedi5 <abcmehedi5@gmail.com>
2024-03-15 10:31:49 +0600

    fixed responsive issues

--- src/components/home/email-subscribe/index.tsx
+++ src/components/home/email-subscribe/index.tsx
@@ -5,17 +5,14 @@ import { Button } from "@/components/ui/button";
 
 const EmailSubscribe = () => {
   return (
-    <div className="bg-white py-12 w-3/4 mx-auto border shadow-md rounded">
+    <div className="bg-white md:py-12 w-3/4 mx-auto border shadow-md rounded">
       <div className="p-3">
-        <h1 className="uppercase text-black text-center mb-3 text-2xl ">
+        <h1 className="uppercase text-black text-center mb-3 md:text-2xl text-sm sm:text-xl ">
           Get the latest deals and more
         </h1>
-        <Button className="absolute right-[240px]" variant={"ghost"}>
-          Subscribe
-        </Button>
-        <div className="flex w-full max-w-sm mx-auto items-center space-x-2">
+        <div className="flex flex-col md:flex-row w-full max-w-sm mx-auto items-center space-x-2">
           <Input type="email" placeholder="Email" />
-          <Button type="submit">Subscribe</Button>
+          <Button className="md:mt-0 mt-3" type="submit">Subscribe</Button>
         </div>
       </div>
     </div>
--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -7,15 +7,14 @@ import EmailSubscribe from "@/components/home/email-subscribe";
 const Footer = () => {
   return (
     <footer className="bg-black text-white py-10 mt-32 w-full relative">
- 
       <div className="container mx-auto mt-20">
-      <div className="absolute right-0 left-0 -top-24">
-        <EmailSubscribe />
-      </div>
+        <div className="absolute right-0 left-0 -top-24">
+          <EmailSubscribe />
+        </div>
 
         {/* row -1  */}
 
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col md:flex-row justify-between items-center">
           <div>
             <img
               className="w-[80px]"
@@ -34,14 +33,14 @@ const Footer = () => {
             <p className="text-[#FE4C06]">support@appleempirebd.com</p>
           </div>
 
-          <div>
+          <div className="md:block hidden">
             <FaAnglesDown className="text-[#FE4C06] h-14 w-14 rounded-full border-2 border-white s p-2 cursor-pointer " />
           </div>
         </div>
         {/* row -2  */}
 
-        <div className="flex justify-between items-center mt-16">
-          <ul className="flex items-center gap-4 font-normal cursor-pointer">
+        <div className="flex flex-col md:flex-row justify-between items-center mt-16">
+          <ul className="sm:flex items-center flex-wrap  gap-4 font-normal cursor-pointer">
             <li>About Us</li>
             <li>Order Tracking</li>
             <li>Return Policy</li>
@@ -73,7 +72,7 @@ const Footer = () => {
 
         {/* row -3  */}
 
-        <div className="flex justify-between items-center mt-16 gap-4">
+        <div className="flex flex-col md:flex-row justify-between items-center mt-16 gap-4 text-center  md:text-start ">
           <div>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
             officia iure adipisci laborum dolore eos exercitationem quis est
