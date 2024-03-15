5deaa6b5d3c4fcca62b66b7d43fee081d4218096
abcmehedi5 <abcmehedi5@gmail.com>
2024-03-15 11:41:30 +0600

    update cover page design

--- src/components/home/hero-section/index..tsx
+++ src/components/home/hero-section/index..tsx
@@ -16,33 +16,53 @@ const HeroSection = () => {
     Autoplay({ delay: 2000, stopOnInteraction: true })
   );
   return (
-    <Carousel
-      plugins={[plugin.current]}
-      className="w-full"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
-    >
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div>
-              <Card>
-                {/* <CardContent className="flex w-full aspect-square items-center justify-center  lg:h-[600px] h-[230px] p-1"> */}
-                <CardContent className="flex w-full aspect-[1920/850] items-center justify-center   p-1">
-                  <img
-                    className="h-full w-full rounded-sm"
-                    src="https://d61s2hjse0ytn.cloudfront.net/images/web/slider/Bkash_S24_Ultra.webp"
-                    alt=""
-                  />
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className="absolute top-[50%] left-2 "  />
-      <CarouselNext className="absolute top-[50%] right-2 " />
-    </Carousel>
+    <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+      <div className="md:col-span-8">
+        <Carousel
+          plugins={[plugin.current]}
+          className="w-full"
+          onMouseEnter={plugin.current.stop}
+          onMouseLeave={plugin.current.reset}
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="aspect-w-16 aspect-h-9">
+                  <Card className="h-full">
+                    <CardContent className="flex h-full">
+                      <img
+                        className=""
+                        src="https://d61s2hjse0ytn.cloudfront.net/images/web/slider/Bkash_S24_Ultra.webp"
+                        alt=""
+                      />
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="absolute top-1/2 transform -translate-y-1/2 left-2" />
+          <CarouselNext className="absolute top-1/2 transform -translate-y-1/2 right-2" />
+        </Carousel>
+      </div>
+
+      <div className="md:col-span-4 grid grid-cols-2 md:grid-cols-1">
+        <div>
+          <img
+            className="object-cover w-full h-full rounded-md"
+            src="https://i.ibb.co/3hPNsm6/photo-2023-12-06-14-59-38.webp"
+            alt=""
+          />
+        </div>
+        <div className="md:mt-3 me-3">
+          <img
+            className="object-cover w-full h-r rounded-md"
+            src="https://i.ibb.co/f0HKFHS/photo-2023-12-06-16-39-32.webp"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
