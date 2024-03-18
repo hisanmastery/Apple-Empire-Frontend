e5f8a2d356573bd83ab70a99b53b4bbefac2801d
abcmehedi5 <abcmehedi5@gmail.com>
2024-03-18 22:55:18 +0600

    added link components

--- src/components/common/product-card/index.tsx
+++ src/components/common/product-card/index.tsx
@@ -34,24 +34,28 @@ const ProductCard = ({ datas }: any) => {
         className="cursor-pointer product-card-one w-full h-full bg-white rounded relative group overflow-hidden hover:scale-105 ease-in-out duration-500"
         style={{ boxShadow: "0px 15px 64px 0px rgba(0, 0, 0, 0.05)" }}
       >
-        <div
-          className="product-card-img w-full h-[300px]"
-          style={{
-            background: `url(${datas?.image}) no-repeat center`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            width: "100%",
-            height: "200px",
-            margin: "auto",
-          }}
-        ></div>
+        <Link href={`/products/${datas?.id}`}>
+          <div
+            className="product-card-img w-full h-[300px]"
+            style={{
+              background: `url(${datas?.image}) no-repeat center`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              width: "100%",
+              height: "200px",
+              margin: "auto",
+            }}
+          ></div>
+        </Link>
         <div className="px-[10px] pb-[10px] ">
           {/* add to cart button */}
           <div className="absolute bottom-1  gap-1 left-1 right-1">
             <button
               disabled={isInCart}
               onClick={() => handleCartClick()}
-              className={`bg-slate-800 uppercase ${!isInCart ? '  hover:bg-[#FF4C06]' : 'bg-slate-500 opacity-40'} rounded ease-in-out duration-500 transition-all w-full text-white p-2 font-normal text-sm`}
+              className={`bg-slate-800 uppercase ${
+                !isInCart ? "  hover:bg-[#FF4C06]" : "bg-slate-500 opacity-40"
+              } rounded ease-in-out duration-500 transition-all w-full text-white p-2 font-normal text-sm`}
             >
               {isInCart ? "Added to cart" : "ADD TO CART"}
             </button>
@@ -62,6 +66,7 @@ const ProductCard = ({ datas }: any) => {
               {datas?.title}
             </p>
           </Link>
+
           {/* rating */}
           <div className="reviews flex space-x-[1px] mb-3"></div>
           <p className="price pb-10">
