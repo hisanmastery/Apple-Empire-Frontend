7a3836b6ae631c5aa5dc631d0a6a9a1107cd32ab
dalim-kazi <mddalim862483@gmail.com>
2024-03-20 19:59:43 +0600

    same

--- src/components/checkout/index.tsx
+++ src/components/checkout/index.tsx
@@ -10,10 +10,15 @@ const Checkout = () => {
     return acc + product?.quantity * parseInt(product?.offer_price);
   }, 0);
   return (
-    <main className=" container mx-auto">
-      <div>
+    <main className=" container mx-auto mt-10">
+      <div className="flex justify-between items-center">
+        <div>
         <h2 className="uppercase text-2xl">Checkout</h2>
         <p>Please enter your details below to complete your purchase</p>
+        </div>
+        <div>
+          <button className="border-2 border-orange-500 text-orange-500 px-5 p-2 rounded-md hover:bg-orange-500 hover:text-white transition-all ease-in-out duration-700">BACK TO CART</button>
+        </div>
       </div>
       <div className="grid grid-cols-3 justify-center gap-8 items-center mt-10">
         <div>
--- src/components/checkout/shiping-address/index.tsx
+++ src/components/checkout/shiping-address/index.tsx
@@ -1,9 +1,70 @@
+"use client";
 import React from "react";
-
+import { useForm } from "react-hook-form";
+import { CiLocationOn } from "react-icons/ci";
+const address = ["jessore", "khulna", "dhaka"];
 const ShippingAddress = () => {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <div>
-      <h1>Shipping address</h1>
+    <div className="border-2 border-gray-400 p-5">
+      <h1 className="flex items-center text-lg font-semibold gap-4 border-b-2 border-gray-400 py-2">
+        <button className="bg-orange-500 p-3 rounded-full">
+          <CiLocationOn className="text-2xl text-white" />
+        </button>
+        Shipping address
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-5">
+        <div>
+          <h1 className="mb-2">Full Name</h1>
+          <input
+            type="text"
+            placeholder="Name"
+            className="border-[1px] w-full p-2 rounded-md focus:outline-none border-gray-400"
+          />
+        </div>
+        <div>
+          <h1 className="mb-2">Address</h1>
+          <textarea
+            placeholder="Address"
+            className="border-[1px] w-full p-2 rounded-md focus:outline-none border-gray-400"
+          />
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="w-full">
+            <select
+              name=""
+              id=""
+              className="border-[1px] w-full p-2 rounded-md focus:outline-none border-gray-400"
+            >
+              {address?.map((item, index) => (
+                <option key={index}>{item}</option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <div className="w-full">
+              <select
+                name=""
+                id=""
+                className="border-[1px] w-full p-2 rounded-md focus:outline-none border-gray-400"
+              >
+                {address?.map((item, index) => (
+                  <option key={index}>{item}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
