a3b044ed553c36c46cedb765dc0500aa935a87ef
homayungit <homayunjust@gmail.com>
2024-03-29 15:20:43 +0600

    fixed responsive issues

--- src/components/home/product-slider/index.tsx
+++ src/components/home/product-slider/index.tsx
@@ -25,17 +25,18 @@ const ProductSlider = ({ productData }: any) => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
             },
+
             {
                 breakpoint: 480,
                 settings: {
