74f19c19d3726e473172837dbdc864a7fd903945
homayungit <homayunjust@gmail.com>
2024-04-01 20:50:46 +0600

    added loading components

---
+++ src/app/(landing-layout)/products/loading.tsx
@@ -0,0 +1,11 @@
+import Loading from "@/components/common/loading";
+
+const ProductLoading = () => {
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
+};
+
+export default ProductLoading;
