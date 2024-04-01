4ca0ce8a6371c002522103a2960987a70bffc44e
homayungit <homayunjust@gmail.com>
2024-04-01 21:23:26 +0600

    added carousel components

--- src/components/common/carousel/index.tsx
+++ src/components/common/carousel/index.tsx
@@ -1,42 +1,70 @@
-import React from "react";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-const MultiCarousel = ({ children }: any) => {
-  return (
-    <div>
-      <Carousel
-        opts={{
-          align: "start",
-        }}
-        className="w-full "
-      >
-        {/* <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent> */}
+import React, { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { icons } from "@/constants/icons";
 
-        <CarouselContent>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            {children}
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+const MultiCarousel = ({ children, settings }: any) => {
+  let sliderRef: any = useRef(null);
+  const next = () => {
+    sliderRef.current.slickNext();
+  };
+  const previous = () => {
+    sliderRef.current.slickPrev();
+  };
+
+  const defaultSettings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
+
+  const mergedSettings = { ...defaultSettings, ...settings };
+
+  return (
+    <div className="container relative">
+      <Slider ref={sliderRef} {...mergedSettings}>
+        {children}
+      </Slider>
+      <div style={{ textAlign: "center" }}>
+        <button
+          className="button absolute  left-8 top-[50%]"
+          onClick={previous}
+        >
+          <icons.previousIcon className="text-2xl text-_primary" />
+        </button>
+        <button className="button absolute  right-8 top-[50%]" onClick={next}>
+          <icons.nextIcon className="text-2xl text-_primary" />
+        </button>
+      </div>
     </div>
   );
 };
