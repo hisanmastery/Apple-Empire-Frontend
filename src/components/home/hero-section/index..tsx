42b1e96cd66b9a6f77cf32e297d3cc045bb9cd10
homayungit <homayunjust@gmail.com>
2024-05-10 18:26:13 +0600

    update carousel

--- src/components/home/hero-section/index..tsx
+++ src/components/home/hero-section/index..tsx
@@ -101,79 +101,107 @@ const HeroSection = () => {
       campaign_product_available: null,
       campaign_product_sale: null,
       product_type: null,
-    }
+    },
   ];
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-12 gap-4 sm:p-3 p-0 xl:container">
-      <div className="md:col-span-12 xl:col-span-8">
-        <Carousel
-          plugins={[plugin.current]}
-          className="w-full"
-          onMouseEnter={plugin.current.stop}
-          onMouseLeave={plugin.current.reset}
-        >
-          <CarouselContent>
-            {products.slice(0, 3).map((product, index) => (
-              <CarouselItem key={index}>
-                <div className="aspect-w-16 aspect-h-9">
-                  <Card className="h-full">
-                    <CardContent className="flex h-full">
-                      <img
-                        className=""
-                        src={product.image}
-                        alt={`Carousel Image ${index + 1}`}
-                      />
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-
-          <CarouselPrevious className="absolute top-1/2 transform -translate-y-1/2 left-2" />
-          <CarouselNext className="absolute top-1/2 transform -translate-y-1/2 right-2" />
-        </Carousel>
-      </div>
-
-      <div className="md:col-span-12 xl:col-span-4 bg-[#D6E6EC] border rounded-lg px-5 flex flex-col justify-center items-center">
-        <h1 className="uppercase text-xl  font-semibold text-center mb-5">
-          🔥 Hot deal of the day 🔥
-        </h1>
-
-        <div className="flex xl:flex-col flex-col md:flex-row gap-4">
-          {hotProducts.map((datas, index) => (
-            <div
-              key={index}
-              className="aspect-w-16 aspect-h-9 bg-white border border-primarygray px-5 py-5 mb-5 rounded-lg flex justify-center items-center gap-4"
-            >
-              <div className="w-[100px] h-[100px] relative">
-                <img
-                  src={datas?.image}
-                  className="w-full h-full rounded-md object-cover"
-                  alt="product"
-                />
-              </div>
-              <div className="flex-1 h-full flex flex-col justify-center">
-                <Link href={`/products/${datas?.id}`}>
-                  <p className="font-semibold title mb-2 sm:text-[15px] text-[13px] font-600 text-qblack leading-[24px] line-clamp-1 hover:text-qyellow cursor-pointer">
-                    {datas?.title}
-                  </p>
-                </Link>
-                <p className="price">
-                  <span className="font-bold main-price text-qgray line-through font-600 text-[18px]">
-                    {datas?.price}
-                  </span>
-                  <span className="font-bold text-red-500 offer-price text-qred font-600 text-[18px] ml-2">
-                    {datas?.offer_price}
-                  </span>
-                </p>
-              </div>
+    <Carousel
+      plugins={[plugin.current]}
+      className="w-full"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent className="">
+        {products.slice(0, 3).map((product, index) => (
+          <CarouselItem key={index}>
+            <div className="aspect-w-16 aspect-h-9">
+              <Card className="h-full">
+                <CardContent className="flex w-full  items-center justify-center  lg:h-[480px] h-full p-1">
+                  <img
+                    className=""
+                    src={product.image}
+                    alt={`Carousel Image ${index + 1}`}
+                  />
+                </CardContent>
+              </Card>
             </div>
-          ))}
-        </div>
-      </div>
-    </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+
+      <CarouselPrevious className="absolute top-1/2 transform -translate-y-1/2 left-2" />
+      <CarouselNext className="absolute top-1/2 transform -translate-y-1/2 right-2" />
+    </Carousel>
+
+    // <div className="grid grid-cols-1 md:grid-cols-12 gap-4 sm:p-3 p-0 xl:container">
+    //   <div className="md:col-span-12 xl:col-span-8">
+    //     <Carousel
+    //       plugins={[plugin.current]}
+    //       className="w-full"
+    //       onMouseEnter={plugin.current.stop}
+    //       onMouseLeave={plugin.current.reset}
+    //     >
+    //       <CarouselContent>
+    //         {products.slice(0, 3).map((product, index) => (
+    //           <CarouselItem key={index}>
+    //             <div className="aspect-w-16 aspect-h-9">
+    //               <Card className="h-full">
+    //                 <CardContent className="flex h-full">
+    //                   <img
+    //                     className=""
+    //                     src={product.image}
+    //                     alt={`Carousel Image ${index + 1}`}
+    //                   />
+    //                 </CardContent>
+    //               </Card>
+    //             </div>
+    //           </CarouselItem>
+    //         ))}
+    //       </CarouselContent>
+
+    //       <CarouselPrevious className="absolute top-1/2 transform -translate-y-1/2 left-2" />
+    //       <CarouselNext className="absolute top-1/2 transform -translate-y-1/2 right-2" />
+    //     </Carousel>
+    //   </div>
+
+    //   <div className="md:col-span-12 xl:col-span-4 bg-[#D6E6EC] border rounded-lg px-5 flex flex-col justify-center items-center">
+    //     <h1 className="uppercase text-xl  font-semibold text-center mb-5">
+    //       🔥 Hot deal of the day 🔥
+    //     </h1>
+
+    //     <div className="flex xl:flex-col flex-col md:flex-row gap-4">
+    //       {hotProducts.map((datas, index) => (
+    //         <div
+    //           key={index}
+    //           className="aspect-w-16 aspect-h-9 bg-white border border-primarygray px-5 py-5 mb-5 rounded-lg flex justify-center items-center gap-4"
+    //         >
+    //           <div className="w-[100px] h-[100px] relative">
+    //             <img
+    //               src={datas?.image}
+    //               className="w-full h-full rounded-md object-cover"
+    //               alt="product"
+    //             />
+    //           </div>
+    //           <div className="flex-1 h-full flex flex-col justify-center">
+    //             <Link href={`/products/${datas?.id}`}>
+    //               <p className="font-semibold title mb-2 sm:text-[15px] text-[13px] font-600 text-qblack leading-[24px] line-clamp-1 hover:text-qyellow cursor-pointer">
+    //                 {datas?.title}
+    //               </p>
+    //             </Link>
+    //             <p className="price">
+    //               <span className="font-bold main-price text-qgray line-through font-600 text-[18px]">
+    //                 {datas?.price}
+    //               </span>
+    //               <span className="font-bold text-red-500 offer-price text-qred font-600 text-[18px] ml-2">
+    //                 {datas?.offer_price}
+    //               </span>
+    //             </p>
+    //           </div>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
