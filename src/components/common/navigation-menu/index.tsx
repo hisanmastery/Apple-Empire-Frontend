e1388de4ecebc2587c5fcd241d0be4f40f15e30f
dalim-kazi <mddalim862483@gmail.com>
2024-05-11 23:50:35 +0600

    new navbar add

---
+++ src/components/common/navigation-menu/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import Link from 'next/link';
+
+const CustomNavigationMenu = ({ data, title }: any) => {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>{title}</NavigationMenuTrigger>
+                    <NavigationMenuContent style={{ width:"200px"}} className='h-auto w-72'>
+                        {
+                            data?.map((items: any, index: number) => <Link key={index} href={""} className='mb-10 hover:text-_primary w-80'>
+                                <p>{items.value}</p>
+                            </Link>)
+                        }
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+
+    );
+};
+
+export default CustomNavigationMenu;
