ebca743245711210f186dbe8a4cfa3b7851d4c02
dalim-kazi <mddalim862483@gmail.com>
2024-05-17 18:10:07 +0600

    cart update

--- src/store/features/cart/cartSlice.tsx
+++ src/store/features/cart/cartSlice.tsx
@@ -17,11 +17,6 @@ const cartSlice = createSlice({
     addStoredCart(state: ICartState, { payload }) {
       // Extracting the IDs from the store array
       const productids = payload?.map((item: any) => item?.id);
-      // Filtering products from datas based on storeIds
-      // const filteredProducts = payload?.filter((product: any) =>
-      //   productids?.includes(product.id)
-      // );
-
       // Extracting the IDs and quantities from the payload
       const payloadData = payload?.reduce((acc: any, item: any) => {
         acc[item.id] = item.quantity || 1;
@@ -35,7 +30,19 @@ const cartSlice = createSlice({
       }));
       // state.storedCart = payload;
     },
+    incrementQuantity(state, { payload }) {
+      const item: any = state.storedCart.find((item: any) => item._id === payload._id);
+      if (item) {
+        item.quantity += 1;
+      }
+    },
+    decrementQuantity(state, { payload }) {
+      const item: any = state.storedCart.find((item: any) => item._id === payload._id);
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      }
+    },
   },
 });
-export const { addcart, addStoredCart } = cartSlice.actions;
+export const { addcart, addStoredCart, incrementQuantity, decrementQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
