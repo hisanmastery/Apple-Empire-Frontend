b50fbe93ea3a305374821f9f81d24a398dc72fcf
homayungit <homayunjust@gmail.com>
2024-05-30 23:21:26 +0600

    reaplace images

--- src/components/home/campaigns-count-down/index.tsx
+++ src/components/home/campaigns-count-down/index.tsx
@@ -69,7 +69,7 @@ export default function CampaignsCountDown({ className, lastDate }: any) {
                 </div>
               </div>
               <div className="w-full lg:h-[400px]  border-2 ">
-                <img className="h-full w-full" src="https://d61s2hjse0ytn.cloudfront.net/images/web/slider/iPhone-15-Series_copy.webp" alt="" />
+                <img className="h-full w-full" src="https://d61s2hjse0ytn.cloudfront.net/images/web/slider/gaming-Laptop.webp" alt="" />
               </div>
 
             </div>
