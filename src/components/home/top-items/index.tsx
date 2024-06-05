dda4fc46fc8ba373523c8e0d89c788d68eed7282
dalim-kazi <mddalim862483@gmail.com>
2024-06-05 22:47:02 +0600

    category update

--- src/components/home/top-items/index.tsx
+++ src/components/home/top-items/index.tsx
@@ -1,20 +1,25 @@
+"use client"
 import React from "react";
 import categories from "@/../../public/category.json";
+import { useGetAllCategoryQuery } from "@/store/features/category/categoryApi";
 const TopItems = () => {
+  const { data }:any = useGetAllCategoryQuery({})
+  console.log(data?.categories);
   return (
     <div className="container mt-10">
       <div className="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-2 gap-4">
-        {categories.top_items.map((category, index) => (
+        {data?.categories?.map((category:any, index:number) => (
           <div
-            className="text-center border rounded shadow p-3 hover:font-bold hover:text-[#FF4C06] hover:border hover:border-[#FF4C06] hover:bg-slate-200 cursor-pointer transition-all ease-in-out duration-500 group"
+            className="text-center rounded shadow p-3 hover:font-bold hover:text-[#FF4C06] hover:border hover:border-[#FF4C06] bg-_white hover:bg-slate-200 cursor-pointer transition-all ease-in-out duration-500 group"
             key={index}
+            style={{ boxShadow: "0px 0px 2px 0px gray" }}
           >
             <img
               className="w-2/4 mx-auto group-hover:scale-125 transition-all ease-in-out duration-700"
               src={category.image}
               alt="category"
             />
-            <h4>{category.name}</h4>
+            <h4>{category.categoryName}</h4>
           </div>
         ))}
       </div>
