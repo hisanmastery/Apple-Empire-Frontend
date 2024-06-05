27e2f67ee6ccaf3b7a3671bee40bcb376133c253
dalim-kazi <mddalim862483@gmail.com>
2024-06-05 22:51:05 +0600

    category update

--- src/components/home/top-items/index.tsx
+++ src/components/home/top-items/index.tsx
@@ -1,16 +1,17 @@
 "use client"
 import React from "react";
-import categories from "@/../../public/category.json";
 import { useGetAllCategoryQuery } from "@/store/features/category/categoryApi";
 const TopItems = () => {
-  const { data }:any = useGetAllCategoryQuery({})
+  const { data }: any = useGetAllCategoryQuery({
+    limit:12
+  })
   console.log(data?.categories);
   return (
     <div className="container mt-10">
       <div className="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-2 gap-4">
         {data?.categories?.map((category:any, index:number) => (
           <div
-            className="text-center rounded shadow p-3 hover:font-bold hover:text-[#FF4C06] hover:border hover:border-[#FF4C06] bg-_white hover:bg-slate-200 cursor-pointer transition-all ease-in-out duration-500 group"
+            className="text-center border-[1px] border-_white rounded shadow p-3 hover:font-bold hover:text-[#FF4C06] hover:border hover:border-[#FF4C06] bg-_white hover:bg-slate-200 cursor-pointer transition-all ease-in-out duration-500 group"
             key={index}
             style={{ boxShadow: "0px 0px 2px 0px gray" }}
           >
