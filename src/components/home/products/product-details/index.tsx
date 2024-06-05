825c87293a791c10e14235dabee01584ecd2e341
dalim-kazi <mddalim862483@gmail.com>
2024-06-05 23:51:47 +0600

    add warranty

--- src/components/home/products/product-details/index.tsx
+++ src/components/home/products/product-details/index.tsx
@@ -147,19 +147,11 @@ const selectedImages = selectedColor
       label: "Warranty",
       content: (
         <div className="text-gray-600">
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-          undoubtable source.
+           {data?.response?.warranty}
         </div>
       ),
     },
   ];
-
-
 
    if (isLoading) {
     return <Loading/>
