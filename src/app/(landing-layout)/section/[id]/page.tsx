bea49d3561bcf70c73982482d18b369f659e969f
dalim-kazi <mddalim862483@gmail.com>
2024-06-10 01:09:26 +0600

    prodcts filter update

--- src/app/(landing-layout)/section/[id]/page.tsx
+++ src/app/(landing-layout)/section/[id]/page.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import AllProductsSection from "@/components/all-products";
 import ProductsSideBar from "@/components/products-sidebar";
 const Products = ({ params }: any) => {
-   let title=""
+   let productsType=""
   if (params?.id) {
     // Decode the URL component and format it
     const formattedCategory = decodeURIComponent(params?.id)
       .split("-")
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
       .join(" ");
-      title=formattedCategory
+      productsType=formattedCategory
   }
   return (
     <div className='grid grid-cols-7 gap-5 container'>
@@ -17,7 +17,7 @@ const Products = ({ params }: any) => {
         <ProductsSideBar />
       </div>
       <div className='col-span-5'>
-        <AllProductsSection productsType={params?.id} title={title } />
+        <AllProductsSection productsType={productsType} />
       </div>
     </div>
   );
