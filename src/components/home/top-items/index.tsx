9da235552b2a9456846a7f5e429569c994efbe8a
dalim-kazi <mddalim862483@gmail.com>
2024-07-14 20:35:06 +0600

    build error fix

--- src/components/home/top-items/index.tsx
+++ src/components/home/top-items/index.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import { useGetAllCategoryQuery } from "@/store/features/category/categoryApi";
 import Link from "next/link";
 import Image from "next/image";
+import Loading from "@/components/common/loading";
 const TopItems = () => {
-  const { data }: any = useGetAllCategoryQuery({
+  const { data ,isLoading}: any = useGetAllCategoryQuery({
     limit:12
   })
+  if (isLoading) return <Loading/>;
   return (
     <div className="container mt-10">
       <div className="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-2 gap-4">
@@ -18,14 +20,14 @@ const TopItems = () => {
             className="text-center border-[1px] border-_white rounded shadow p-3 hover:font-bold hover:text-[#FF4C06] hover:border hover:border-[#FF4C06] bg-_white hover:bg-slate-200 cursor-pointer transition-all ease-in-out duration-500 group"
             style={{ boxShadow: "0px 0px 2px 0px gray" }}
           >
-              <Image
+              <img
                 width={100}
                 height={100}
               className="w-2/4 mx-auto group-hover:scale-125 transition-all ease-in-out duration-700"
-              src={category.image}
+              src={category?.image}
               alt="category"
             />
-            <h4>{category.categoryName}</h4>
+            <h4>{category?.categoryName}</h4>
           </div>
           </Link>
         ))}
