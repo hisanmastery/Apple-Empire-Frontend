be626e6f6bb8ca46d03c35a781924fa5c5fb93c8
homayungit <homayunjust@gmail.com>
2024-09-03 21:48:08 +0600

    added middlebar

--- src/app/(landing-layout)/layout.tsx
+++ src/app/(landing-layout)/layout.tsx
@@ -3,12 +3,14 @@ import Carts from "@/components/carts";
 import Footer from "@/components/shared/footer/footer";
 import Navbar from "@/components/shared/navbar";
 import NewNavbar from "@/components/shared/new-navbar";
+import Middlebar from "@/components/shared/new-navbar/MiddleNavbar";
 import useAuth from "@/hooks/useAuth";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       {/* <Navbar /> */}
+      <Middlebar/>
       <NewNavbar />
       {/* <Carts /> */}
       {/* children content */}
