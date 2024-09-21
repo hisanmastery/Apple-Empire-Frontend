9482f14e5104f1143c193ed68142c953341d3af0
homayungit <homayunjust@gmail.com>
2024-09-22 00:06:19 +0600

    added enpoint to env

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,2 +1,2 @@
-export const baseApiUrl = "https://api.appleempirebd.com/v1/api";
+export const baseApiUrl = `${process.env.NEXT_PUBLIC_API_URL}/v1/api`;
 // export const baseApiUrl = "http://localhost:5000/v1/api";
