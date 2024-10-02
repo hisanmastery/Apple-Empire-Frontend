58403f2b8409121c9d9648f40a8ef676ee395d16
dalim-kazi <mddalim862483@gmail.com>
2024-10-02 22:43:53 +0600

    search implement

--- src/components/shared/navbar/searchInput.tsx
+++ src/components/shared/navbar/searchInput.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { Search } from 'lucide-react';
 
 const SearchInput = () => {
--- src/components/shared/new-navbar/SearchBox.tsx
+++ src/components/shared/new-navbar/SearchBox.tsx
@@ -1,19 +1,51 @@
 import { icons } from "@/constants/icons";
-import { useState } from "react";
+import { useGetProductsListsQuery } from "@/store/features/products/productsApi";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
 export default function SearchBox() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const { data } = useGetProductsListsQuery<any>(
+    {
+      searchText: searchTerm,
+    },
+    { skip: !searchTerm }
+  );
 
-  const handleSearch = () => {
-    console.log(searchTerm);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+    setOpen(e.target.value.length > 0);
   };
+
+  const handleSearch = () => {};
+  const productsData = data?.blogs;
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setOpen(false);
+      setSearchTerm("");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <form className="flex items-center w-full relative">
       <input
         type="text"
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={handleInputChange}
         className="w-full p-2 focus:outline-none rounded-full focus:ring-2 focus:ring-_primary transition-all duration-300 ease-in-out"
         placeholder="Search for products..."
         aria-label="Search"
@@ -25,6 +57,34 @@ export default function SearchBox() {
       >
         <icons.SearchIcons className="text-xl" />
       </button>
+      {/* Conditional rendering of search results */}
+      {productsData?.length > 0 && searchTerm && open && (
+        <div
+          className="absolute top-12  z-10 w-full bg-white border border-gray-300 rounded-lg shadow-md max-h-96 overflow-auto"
+          ref={dropdownRef}
+        >
+          {productsData.map((product: any) => (
+            <Link href={`/products/${product?._id}`} key={product._id}>
+              <div
+                className="p-2 group hover:bg-_primary cursor-pointer flex gap-3 items-center"
+                onClick={() => setOpen(false)}
+              >
+                <Image
+                  src={product?.image?.viewUrl}
+                  alt={product?.image?.altText}
+                  width={50}
+                  height={50}
+                  className="object-fill"
+                  quality={100}
+                />
+                <h4 className="group-hover:text-white group-hover:underline">
+                  {product.title}
+                </h4>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </form>
   );
 }
