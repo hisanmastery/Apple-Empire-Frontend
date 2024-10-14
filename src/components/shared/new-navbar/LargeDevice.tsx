69e96badca270c1ebcf1e2b817dbcb551c0f5eb7
githameem <githameem@gmail.com>
2024-10-15 02:10:46 +0600

    offers page linkup correction

--- src/components/shared/new-navbar/LargeDevice.tsx
+++ src/components/shared/new-navbar/LargeDevice.tsx
@@ -123,7 +123,7 @@ export default function LargeDevice({ type }: any) {
                     <Link href={"/section/used"}>Used Products</Link>
                   </div>
                   <div className="text-_black text-md">
-                    <Link href={"/section/offers"}>Offers</Link>
+                    <Link href={"/offers"}>Offers</Link>
                   </div>
                 </div>
               </div>
