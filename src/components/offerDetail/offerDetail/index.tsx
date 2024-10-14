974117ba1fe03890e678a99662bc684376ca3867
githameem <githameem@gmail.com>
2024-10-15 01:47:31 +0600

    offer details page redesign

--- src/components/offerDetail/offerDetail/index.tsx
+++ src/components/offerDetail/offerDetail/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import DiscountBanner from "../discount";
 import DiscountDetails from "../discountDetails";
 import Products from "../products"
 
@@ -13,9 +12,6 @@ const OfferDetail: React.FC<OfferDetailProps> = ({ params }) => {
   const { id } = params;
   return (
     <div>
-      {/* <h1>offer id: {id}</h1>
-      <h1>Hello</h1> */}
-      <DiscountBanner/>
       <DiscountDetails/>
       <Products />
       
