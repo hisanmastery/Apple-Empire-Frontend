591c385a86d32510ac0e826b48d868663301cb7f
githameem <githameem@gmail.com>
2024-10-15 22:08:49 +0600

    skeleton add

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -10,6 +10,7 @@ import Pagination from "../common/pagination";
 import { selectProductsVariant } from "@/store/features/products/productsCategorySlice";
 import SubCategoryTabs from "../category-tabs/SubCategoryTab";
 import { useSearchParams } from "next/navigation";
+import ProductCardSkeleton from "../shared/skeleton/products-card-skeleton";
 interface CategoryProductsProps {
   category: string;
   subCategory: string;
@@ -39,7 +40,13 @@ const CategoryProducts: React.FC<CategoryProductsProps> = ({
 
   //Loading
   if (isLoading) {
-    return <Loading />;
+    return (
+      <div className="grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-1 xmd:grid-cols-2  mx-auto mb-10 mt-10 gap-5 ssm:px-3 msm:px-8 lsm:px-12 xmd:px-0">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <ProductCardSkeleton key={index} />
+        ))}
+      </div>
+    );
   }
 
   return (
