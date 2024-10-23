09b6fc83826bdedbbbef96a543dce1a092793faf
githameem <githameem@gmail.com>
2024-10-24 01:34:16 +0600

    compare page responsive

--- src/components/home/email-subscribe/index.tsx
+++ src/components/home/email-subscribe/index.tsx
@@ -21,23 +21,23 @@ const EmailSubscribe = () => {
             <div className="text-center">
               {/* <icons.loveHartIcon className="text-3xl  bg-slate-200 hover:bg-_primary text-_primary hover:text-_secondary-text w-14 h-14 lsm:w-20 lsm:h-20 p-3 lsm:p-5 mx-auto rounded-full flex justify-center items-center  ease-in-out duration-500 transition-all" /> */}
               <Image className="text-3xl  bg-slate-200 hover:bg-_primary text-_primary hover:text-_secondary-text w-14 h-14 lsm:w-20 lsm:h-20 p-3 lsm:p-5 mx-auto rounded-full flex justify-center items-center  ease-in-out duration-500 transition-all" src={images.onlineSupportIcon} alt="online support" width={50} height={50}/>
-              <h5 className="mt-4">Online Support</h5>
+              <h5 className="text-sm md:text-lg mt-4">Online Support</h5>
             </div>
             <div className="text-center">
               {/* <icons.giftIcon className="text-3xl  bg-slate-200 hover:bg-_primary text-_primary hover:text-_secondary-text w-14 h-14 lsm:w-20 lsm:h-20 p-3 lsm:p-5 mx-auto rounded-full flex justify-center items-center  ease-in-out duration-500 transition-all" /> */}
               <Image className="text-3xl  bg-slate-200 hover:bg-_primary text-_primary hover:text-_secondary-text w-14 h-14 lsm:w-20 lsm:h-20 p-3 lsm:p-5 mx-auto rounded-full flex justify-center items-center  ease-in-out duration-500 transition-all" src={images.officialProductIcon} alt="online support" width={50} height={50}/>
-              <h5 className="mt-4">Official Product</h5>
+              <h5 className="text-sm md:text-lg mt-4">Official Product</h5>
             </div>
             <div className="text-center">
               {/* <icons.truckDelivaryIcon className="text-3xl  bg-slate-200 hover:bg-_primary text-_primary hover:text-_secondary-text w-14 h-14 lsm:w-20 lsm:h-20 p-3 lsm:p-5 mx-auto rounded-full flex justify-center items-center  ease-in-out duration-500 transition-all" /> */}
               <Image className="text-3xl  bg-slate-200 hover:bg-_primary text-_primary hover:text-_secondary-text w-14 h-14 lsm:w-20 lsm:h-20 p-3 lsm:p-5 mx-auto rounded-full flex justify-center items-center  ease-in-out duration-500 transition-all" src={images.fastDeliveryIcon} alt="online support" width={50} height={50}/>
-              <h5 className="mt-4">Fastest Delivery</h5>
+              <h5 className="text-sm md:text-lg mt-4">Fastest Delivery</h5>
             </div>
             <div className="text-center">
               <icons.paymentIcon className="text-3xl  bg-slate-200 hover:bg-_primary text-_primary hover:text-_secondary-text w-14 h-14 lsm:w-20 lsm:h-20 p-3 lsm:p-5 mx-auto rounded-full flex justify-center items-center  ease-in-out duration-500 transition-all" />
               {/* <Image className="text-3xl  bg-slate-200 hover:bg-_primary text-_primary hover:text-_secondary-text w-14 h-14 lsm:w-20 lsm:h-20 p-3 lsm:p-5 mx-auto rounded-full flex justify-center items-center  ease-in-out duration-500 transition-all" src={images.fastDeliveryIcon} alt="online support" width={50} height={50}/> */}
               
-              <h5 className="mt-4">Secure Payment</h5>
+              <h5 className="text-sm md:text-lg mt-4">Secure Payment</h5>
             </div>
           </div>
         </div>
