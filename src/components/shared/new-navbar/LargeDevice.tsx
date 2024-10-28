0e8f11612a7f377d4eaa36ba75e66fe69d1ac3c2
dalim-kazi <mddalim862483@gmail.com>
2024-10-28 20:51:20 +0600

    categorty

--- src/components/shared/new-navbar/LargeDevice.tsx
+++ src/components/shared/new-navbar/LargeDevice.tsx
@@ -41,74 +41,72 @@ export default function LargeDevice({ type }: any) {
             <div className="w-full h-full flex justify-center items-center">
               {/*desktop view*/}
               <div className="category-and-nav lg:flex hidden space-x-3 items-center">
-                {categoriesData?.categories?.map(
-                  (category: any, index: number) => (
-                    <div
-                      key={index}
-                      className="category px-2 rounded-t-md relative"
-                      onMouseEnter={() => handleMouseEnter(category._id)}
-                      onMouseLeave={handleMouseLeave}
-                    >
-                      {/* If category has subcategories, show toggle button */}
-                      {category.subCategory?.length > 0 ? (
-                        <>
-                          <div className="w-full h-full flex justify-between text-center items-center">
-                            <div className="flex space-x-3 items-center">
-                              <Link href={`/category/${category.categoryName}`}>
-                                <span className="text-md font-600 hover:text-_orange text-qblacktext">
-                                  {category.categoryName}
-                                </span>
-                              </Link>
-                            </div>
-                          </div>
-
-                          {/* Category Dropdown */}
-                          {openCategoryId === category._id && (
-                            <div className="category-dropdown max-h-[calc(100vh-200px)] overflow-auto w-full min-w-48 absolute left-0 top-[43px]">
-                              <ul className="categories-list">
-                                {category.subCategory.map(
-                                  (subCategory: string, subIndex: number) => (
-                                    <li
-                                      className="relative group border-b"
-                                      key={subIndex}
-                                    >
-                                      <Link
-                                        href={`/category/${category.categoryName}?subcategory=${subCategory}`}
-                                      >
-                                        <div
-                                          className={`flex justify-between items-center px-2 h-10 bg-_white transition-all duration-300 ease-in-out cursor-pointer text-qblack ${
-                                            type === 3
-                                              ? "hover:bg-qh3-blue hover:text-white"
-                                              : "hover:bg-gray-100 hover:text-_orange"
-                                          }`}
-                                        >
-                                          <span className="text-md font-400">
-                                            {subCategory}
-                                          </span>
-                                        </div>
-                                      </Link>
-                                    </li>
-                                  )
-                                )}
-                              </ul>
-                            </div>
-                          )}
-                        </>
-                      ) : (
-                        // Direct link if no subcategories
-                        <Link href={`/category/${category.categoryName}`}>
-                          <div className="w-full h-full flex justify-between text-center items-center cursor-pointer">
-                            <div className="flex space-x-3 items-center">
+                {categoriesData?.data?.map((category: any, index: number) => (
+                  <div
+                    key={index}
+                    className="category px-2 rounded-t-md relative"
+                    onMouseEnter={() => handleMouseEnter(category._id)}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    {/* If category has subcategories, show toggle button */}
+                    {category.subCategory?.length > 0 ? (
+                      <>
+                        <div className="w-full h-full flex justify-between text-center items-center">
+                          <div className="flex space-x-3 items-center">
+                            <Link href={`/category/${category.categoryName}`}>
                               <span className="text-md font-600 hover:text-_orange text-qblacktext">
                                 {category.categoryName}
                               </span>
-                            </div>
+                            </Link>
                           </div>
-                        </Link>
-                      )}
-                    </div>
-                  )
-                )}
+                        </div>
+
+                        {/* Category Dropdown */}
+                        {openCategoryId === category._id && (
+                          <div className="category-dropdown max-h-[calc(100vh-200px)] overflow-auto w-full min-w-48 absolute left-0 top-[43px]">
+                            <ul className="categories-list">
+                              {category.subCategory.map(
+                                (subCategory: string, subIndex: number) => (
+                                  <li
+                                    className="relative group border-b"
+                                    key={subIndex}
+                                  >
+                                    <Link
+                                      href={`/category/${category.categoryName}?subcategory=${subCategory}`}
+                                    >
+                                      <div
+                                        className={`flex justify-between items-center px-2 h-10 bg-_white transition-all duration-300 ease-in-out cursor-pointer text-qblack ${
+                                          type === 3
+                                            ? "hover:bg-qh3-blue hover:text-white"
+                                            : "hover:bg-gray-100 hover:text-_orange"
+                                        }`}
+                                      >
+                                        <span className="text-md font-400">
+                                          {subCategory}
+                                        </span>
+                                      </div>
+                                    </Link>
+                                  </li>
+                                )
+                              )}
+                            </ul>
+                          </div>
+                        )}
+                      </>
+                    ) : (
+                      // Direct link if no subcategories
+                      <Link href={`/category/${category.categoryName}`}>
+                        <div className="w-full h-full flex justify-between text-center items-center cursor-pointer">
+                          <div className="flex space-x-3 items-center">
+                            <span className="text-md font-600 hover:text-_orange text-qblacktext">
+                              {category.categoryName}
+                            </span>
+                          </div>
+                        </div>
+                      </Link>
+                    )}
+                  </div>
+                ))}
                 <div className="lg:flex items-center gap-10 hidden">
                   <div className="text-_black text-md">
                     <Link href={"/about-us"}>About</Link>
