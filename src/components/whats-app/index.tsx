179ecb5acf41e8a6849cc091c6d2461f624a2a5c
dalim-kazi <mddalim862483@gmail.com>
2024-11-11 22:16:18 +0600

    contery code add

--- src/components/whats-app/index.tsx
+++ src/components/whats-app/index.tsx
@@ -5,11 +5,13 @@ import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 const WhatsApp = () => {
   return (
-    <FloatingWhatsApp
-      phoneNumber="01616436310"
-      accountName="Apple Empire"
-      avatar={images.NavbarLogo.src || ""}
-    />
+    <div className="relative">
+      <FloatingWhatsApp
+        phoneNumber="+8801616436310"
+        accountName="Apple Empire"
+        avatar={images.NavbarLogo.src || ""}
+      />
+    </div>
   );
 };
 
