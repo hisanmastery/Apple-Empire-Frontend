933379961ba88aff9f2373247ef5860e50677ea2
dalim-kazi <mddalim862483@gmail.com>
2024-12-06 15:13:08 +0600

    next config file modify

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "static.thenounproject.com",
-      "t4.ftcdn.net",
-      "example.com",
-      "snapstore.nusaiba.com.bd",
-      "img.freepik.com",
-      "www.iamekarting.com",
-      "i.ibb.co",
-      "i.ibb.co.com",
-      "34.121.66.25",
-      "cdn.pixabay.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
     ],
   },
 };
